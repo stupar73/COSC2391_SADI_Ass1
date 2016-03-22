ce25d3bdafdfa78c2c1421e8d09eae70ce7bf3bc
Stuart Parker <stu_par73@hotmail.com>
2016-03-22 22:22:55 +1100

    Started implementing methods

--- src/model/GameEngineImpl.java
+++ src/model/GameEngineImpl.java
@@ -1,17 +1,31 @@
 package model;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Random;
 import model.interfaces.GameEngine;
 import model.interfaces.GameEngineCallback;
 import model.interfaces.Player;
 
 public class GameEngineImpl implements GameEngine
 {
+    private Collection<Player> players;
+    private Collection<GameEngineCallback> gameEngineCallbacks;
 
     @Override
     public int spin(int wheelSize, int initialDelay, int finalDelay,
-                    int delayIncrement)
+            int delayIncrement)
     {
+        Random r = new Random();
+
+        int randNum = r.nextInt(wheelSize) + 1;
+
+        for (int delay = initialDelay; delay < finalDelay; delay +=
+                delayIncrement)
+        {
+
+        }
+
         // TODO Auto-generated method stub
         return 0;
     }
@@ -19,28 +33,43 @@ public class GameEngineImpl implements GameEngine
     @Override
     public void addPlayer(Player player)
     {
-        // TODO Auto-generated method stub
+        // TODO check input values
 
+        if (!players.add(player))
+        {
+            // TODO Player not added to collection 'players'
+        }
     }
 
     @Override
     public boolean removePlayer(Player player)
     {
-        // TODO Auto-generated method stub
-        return false;
+        // TODO check input values
+
+        return players.remove(player);
     }
 
     @Override
     public Player getPlayer(String id)
     {
-        // TODO Auto-generated method stub
+        for (Player player : players)
+        {
+            if (player.getPlayerId().equals(id))
+            {
+                return player;
+            }
+        }
         return null;
     }
 
     @Override
     public boolean placeBet(Player player, int number, int bet)
     {
-        // TODO Auto-generated method stub
+        // TODO check input values
+
+        // TODO Check the return
+        player.placeBet(number, bet);
+
         return false;
     }
 
@@ -54,22 +83,23 @@ public class GameEngineImpl implements GameEngine
     @Override
     public Collection<Player> getAllPlayers()
     {
-        // TODO Auto-generated method stub
-        return null;
+        return Collections.unmodifiableCollection(players);
     }
 
     @Override
     public void addGameEngineCallback(GameEngineCallback gameEngineCallback)
     {
-        // TODO Auto-generated method stub
+        // TODO check input values
 
+        gameEngineCallbacks.add(gameEngineCallback);
     }
 
     @Override
     public void removeGameEngineCallback(GameEngineCallback gameEngineCallback)
     {
-        // TODO Auto-generated method stub
+        // TODO check input values
 
+        gameEngineCallbacks.remove(gameEngineCallback);
     }
 
 }
--- src/model/SimplePlayer.java
+++ src/model/SimplePlayer.java
@@ -4,73 +4,108 @@ import model.interfaces.Player;
 
 public class SimplePlayer implements Player
 {
+    private String id;
+    private String name;
+    private int points;
+    private int bet;
+    public int number;
 
-    public SimplePlayer(String string, String string2, int i)
+    public SimplePlayer(String id, String name, int points)
     {
-        // TODO Auto-generated constructor stub
+        this.id = id;
+        this.name = name;
+        this.points = points;
+        this.bet = 0;
+        this.number = 0;
     }
 
     @Override
     public String getPlayerName()
     {
-        // TODO Auto-generated method stub
-        return null;
+        return this.name;
     }
 
     @Override
     public void setPlayerName(String playerName)
     {
-        // TODO Auto-generated method stub
-
+        this.name = playerName;
     }
 
     @Override
     public int getPoints()
     {
-        // TODO Auto-generated method stub
-        return 0;
+        return this.points;
     }
 
     @Override
     public void setPoints(int points)
     {
-        // TODO Auto-generated method stub
-
+        this.points = points;
     }
 
     @Override
     public String getPlayerId()
     {
-        // TODO Auto-generated method stub
-        return null;
+        return this.id;
     }
 
     @Override
     public boolean placeBet(int number, int bet)
     {
-        // TODO Auto-generated method stub
-        return false;
+        if (!canPlaceBet(number, bet))
+        {
+            return false;
+        }
+
+        this.points -= bet;
+        this.number = number;
+        this.bet = bet;
+
+        return true;
+    }
+
+    /**
+     * @param number
+     *            the "lucky" number on the wheel the player is betting on
+     * @param bet
+     *            the bet in points
+     * @return true if the player has sufficient points and the bet is greater
+     *         than zero
+     */
+    private boolean canPlaceBet(int number, int bet)
+    {
+        return bet > 0 && this.points <= bet;
     }
 
     @Override
     public int getBet()
     {
-        // TODO Auto-generated method stub
-        return 0;
+        return this.bet;
     }
 
     @Override
     public void resetBet()
     {
-        // TODO Auto-generated method stub
-
+        this.bet = 0;
     }
 
     @Override
     public int getNumber()
     {
-        // TODO Auto-generated method stub
-        return 0;
+        return this.number;
     }
 
+    @Override
+    public String toString()
+    {
+        String s;
+
+        s = "ID: " + this.id;
+        s += "Name: " + this.name;
+        s += "Current points: " + this.points;
+        s += "Current bet: " + this.bet;
+        s += "Lucky number: " + this.number;
+
+        return s;
+    }
 }
